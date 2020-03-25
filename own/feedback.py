30fc87ce9f1abe71b50a8da69a5f2366daeac7bb
Qi Haodi <haodi.qi.2017@business.smu.edu.sg>
2020-03-25 18:01:33 +0800

    Update feedback.py
    Import the datetime library

--- own/feedback.py
+++ own/feedback.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
-import pika, os,json, pytz
+import pika, os, json, pytz
+
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/restaurantCity_feedback'
