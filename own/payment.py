988190c22b577fd6a6cf49bd4d53cbcb2b345cc6
casperlim2018 <casper.lim.2018@sis.smu.edu.sg>
2020-04-02 03:29:52 +0800

    transfer paypal to main

--- own/payment.py
+++ own/payment.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import requests
 
 import pika, json, requests, uuid, pytz
 
@@ -31,21 +32,35 @@ def send_order(order): # only when paypal payment succeed
 def pay_order():
     # Assuming payment successful
     order = request.get_json()
-    order['orderStatus']='paid'
-    order['orderId'] = generate_order_id()
-    order['orderDatetime'] = datetime.now(tz).strftime(format='%Y-%m-%d %H:%M:%S')
-    print(order)
-    send_order(order)
-    return jsonify(order), 200
-
+    return order, 200
+    # print(order)
+    # send_order(order)
+    # return jsonify(order), 200
     # TODO: integrating with Paypal
     # Call paypal api here 
-    # r = requests.post('http://127.0.0.1/7000/checkout',json=order)
+    # r = requests.post('http://127.0.0.1:7000/checkout',json=order)
     # if r.status_code == 200:
+    #     print(order)
     #     send_order(order)
     #     return jsonify(order), 200
     # else:
     #     return jsonify(order), 400
+@app.route('/successpayment',methods=['POST'])
+def success_order():
+    if request.is_json:
+        order = request.get_json()
+        order['orderStatus']='paid'
+        order['orderId'] = generate_order_id()
+        order['orderDatetime'] = datetime.now(tz).strftime(format='%Y-%m-%d %H:%M:%S')
+    else:
+        order = request.get_data()
+        print("Received an invalid order:")
+        print(order)
+        replymessage = json.dumps({"message": "Order should be in JSON", "data": order}, default=str)
+        return replymessage, 400 # Bad Request
+    print("Received an order log by " + __file__)
+    send_order(order)
+    return jsonify('Successfully paid'), 201
     
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=5555,debug=True)
