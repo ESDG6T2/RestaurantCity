bfb0748fd7cf571cf963690bac97ba7c24206b49
Qi Haodi <haodi.qi.2017@business.smu.edu.sg>
2020-04-02 22:02:52 +0800

    Upload telebot.py with dbhelper.py

---
+++ own/dbhelper.py
@@ -0,0 +1,32 @@
+import mysql.connector
+
+class DBHelper:
+    def __init__(self,dbname,pwd=''):
+        self.conexion = mysql.connector.connect(user='root',
+                                                password=pwd,
+                                                host='localhost',
+                                                db=dbname)
+        self.cursor = self.conexion.cursor(buffered=True)
+    def end(self):
+        self.conexion.close()
+#         self.cursor.close() # The code works if i remove this line;
+    def get_user_orders(self,userid):
+        sql = 'SELECT * FROM `order` WHERE `userid` = "{}"'.format(userid)
+        self.cursor.execute(sql)
+        columns = self.cursor.column_names
+        result = [{columns[i]:v for i, v in enumerate(element)} for element in self.cursor.fetchall()]
+        
+        return result
+
+    def get_order_items(self,orderId):
+        sql = 'SELECT * FROM `orderdetail` WHERE `orderId` = "{}"'.format(orderId)
+        self.cursor.execute(sql)
+        columns = self.cursor.column_names
+        result = [{columns[i]:v for i, v in enumerate(element)} for element in self.cursor.fetchall()]
+        
+        return result
+
+    
+
+
+
---
+++ own/telebot.py
@@ -0,0 +1,102 @@
+import json
+import requests
+import time
+import urllib
+
+from dbhelper import DBHelper
+
+db = DBHelper('restaurantcity_order')
+
+TOKEN = '1146167386:AAFuj5hc4FV_YXn1c5Unwtfq-EvqUMC7EEU'
+URL = "https://api.telegram.org/bot{}/".format(TOKEN)
+
+menuItems = json.loads(requests.get("http://127.0.0.1:5001/menu").content)['menu']
+menuDict = {x['menuId']: x['foodName'] for x in menuItems}
+
+def get_url(url):
+    response = requests.get(url)
+    content = response.content.decode("utf8")
+    return content
+
+
+def get_json_from_url(url):
+    content = get_url(url)
+    js = json.loads(content)
+    return js
+
+
+def get_updates(offset=None):
+    url = URL + "getUpdates"
+    if offset:
+        url += "?offset={}".format(offset)
+    js = get_json_from_url(url)
+    return js
+
+
+def get_last_update_id(updates):
+    update_ids = []
+    for update in updates["result"]:
+        update_ids.append(int(update["update_id"]))
+    return max(update_ids)
+
+
+def handle_updates(updates):
+    for update in updates["result"]:
+        text = update["message"]["text"]
+        chat = update["message"]["chat"]["id"]
+        
+        if text == "/check_status":
+            send_message("Please enter your userid starting with @ (e.g. @userid)", chat)
+        elif text == "/start":
+            send_message("Welcome to Restaurant City", chat)
+        elif text[0] == '@':
+            reply_msg = 'Dear {}, thank you for choosing Restaurant City. Your order information:\n'.format(text[1:])
+
+            userid = text[1:]
+            all_orders = db.get_user_orders(userid)
+
+            if len(all_orders) > 1:
+                for i, order in enumerate(all_orders):
+                    orderItems = db.get_order_items(order['orderId'])
+                    order['orderItem'] = orderItems
+                    order_info = format_order_info(i+1, order) 
+                    reply_msg += order_info  
+            else:
+                reply_msg = 'Dear {}, you have no orders placed'.format(text[1:])
+            send_message(reply_msg, chat)
+
+
+def get_last_chat_id_and_text(updates):
+    num_updates = len(updates["result"])
+    last_update = num_updates - 1
+    text = updates["result"][last_update]["message"]["text"]
+    chat_id = updates["result"][last_update]["message"]["chat"]["id"]
+    return (text, chat_id)
+
+
+def send_message(text, chat_id, reply_markup=None):
+    text = urllib.parse.quote_plus(text)
+    url = URL + "sendMessage?text={}&chat_id={}&parse_mode=Markdown".format(text, chat_id)
+    if reply_markup:
+        url += "&reply_markup={}".format(reply_markup)
+    get_url(url)
+
+def format_order_info(idx, order):
+    order_info = 'Order {i}:\nDatetime: {datetime}\nStatus: {status}\nOrder items and quantity:\n'.format(i=idx,status=order['orderStatus'],datetime=order['datetime'])
+    for i, item in enumerate(order['orderItem']):
+        order_info += "{}. {} : {}".format(i+1, menuDict[item['menuId']],item['quantity']) + '\n'
+    order_info += '-'*50+'\n'
+    return order_info
+
+def main():
+    last_update_id = None
+    print("before loop")
+    while True:
+        updates = get_updates(last_update_id)
+        if len(updates["result"]) > 0:
+            last_update_id = get_last_update_id(updates) + 1
+            handle_updates(updates)        
+        time.sleep(0.5)
+
+if __name__ == '__main__':
+    main()
